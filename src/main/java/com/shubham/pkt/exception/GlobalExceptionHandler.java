a3882c0fed23934a2c5361d3d2558e1c418b508d
Shubham K Biradar <shubhamkbiradar@gmail.com>
2026-01-19 23:46:30 +0530

    Added Centralization exception for springboot learning

---
+++ src/main/java/com/shubham/pkt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.shubham.pkt.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidation(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+        return Map.of(
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "details", errors
+        );
+    }
+}
