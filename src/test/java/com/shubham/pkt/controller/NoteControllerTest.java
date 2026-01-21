53febb47a3e1fc00efd2b5ca4be08d045f20133d
Shubham K Biradar <shubhamkbiradar@gmail.com>
2026-01-21 10:10:10 +0530

    Testing of all possible scenarios where test should insert update and fail for blank

--- src/test/java/com/shubham/pkt/controller/NoteControllerTest.java
+++ src/test/java/com/shubham/pkt/controller/NoteControllerTest.java
@@ -1,12 +1,16 @@
 package com.shubham.pkt.controller;
 
+import com.shubham.pkt.exception.NoteNotFoundException;
 import com.shubham.pkt.model.Note;
 import com.shubham.pkt.service.NoteService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,6 +38,34 @@ public class NoteControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.content").value("test"));
+    }
+    @Test
+    void getNoteById_shouldReturn404_whenNoteNotFound() throws Exception {
+
+        when(noteService.findById(99L))
+                .thenThrow(new NoteNotFoundException("99"));
+
+        mockMvc.perform(get("/notes/99"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.details")
+                        .value("Note not found with id: 99"));
+    }
+    @Test
+    void createNote_shouldReturn400_whenContentBlank() throws Exception {
+
+        String body = """
+        {
+          "content": ""
+        }
+        """;
+
+        mockMvc.perform(post("/notes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.details[0]")
+                        .value("content: must not be blank"));
     }
 
 }
